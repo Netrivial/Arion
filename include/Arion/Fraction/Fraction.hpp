e6c90aa60f9d2d2b01974fa0eb5303a97be6e304
Kto-to <makemarat2021@gmail.com>
2026-09-06 03:46:21 +0500

    The first half of the Fraction module is done

--- include/Arion/Fraction/Fraction.hpp
+++ include/Arion/Fraction/Fraction.hpp
@@ -2,6 +2,7 @@
 
 #pragma once
 #include <iostream>
+#include <stdexcept>
 #include <cmath>
 #include <numeric>
 
@@ -13,10 +14,10 @@ namespace Arion {
 			double Denominator;
 
 		public:
-				// Constructs
+				// Constructors
 			Fraction() = delete;
 			Fraction(double Numerator, double Denominator);
-			Fraction(Fraction& other);
+			Fraction(const Fraction& other);
 			// Fraction(Fraction&& other) noexcept;
 
 
@@ -34,6 +35,74 @@ namespace Arion {
 			double Division() const noexcept;
 			void Reducing();
 			void Show() const;
+
+
+				// Operations with fracions
+			friend std::ostream& operator<<(std::ostream& os, const Fraction& f) {
+				os << f.Numerator << "/" << f.Denominator;
+				return os;
+			}
+
+			Fraction& operator=(const Fraction& other);
+			
+			Fraction operator-() const;
+			Fraction& operator++();
+			Fraction operator++(int);
+			Fraction& operator--();
+			Fraction operator--(int);
+
+			Fraction operator+(Fraction other);
+
+			template <typename T>
+			friend auto operator+(const Fraction& frac, const T& other) {
+				return frac.Division() + other;
+			}
+
+			template <typename T>
+			friend auto operator+(const T& other, const Fraction& frac) {
+				return frac.Division() + other;
+			}
+
+			Fraction operator-(Fraction other);
+
+			template <typename T>
+			friend auto operator-(const Fraction& frac, const T& other) {
+				return frac.Division() - other;
+			}
+
+			template <typename T>
+			friend auto operator-(const T& other, const Fraction& frac) {
+				return other - frac.Division();
+			}
+
+			Fraction operator*(Fraction other);
+
+			template <typename T>
+			friend auto operator*(const Fraction& frac, const T&) {
+				return frac.Numerator * T / frac.Denominator;
+			}
+
+			template <typename T>
+			friend auto operator*(const T&, const Fraction& frac) {
+				return frac.Numerator * T / frac.Denominator;
+			}
+
+			Fraction operator/(Fraction other);
+
+			template <typename T>
+			friend auto operator/(const Fraction& frac, const T&) {
+				frac.Numerator / (frac.Denominator * T);
+			}
+
+			template <typename T>
+			friend auto operator/(const T&, const Fraction& frac) {
+				T * frac.Numerator 
+			}
 		};
+
+		template <typename T>
+		Fraction GetFraction(T value) {
+			return Fraction { value, 1 };
+		}
 	}
 }
--- src/Fraction/Fraction.cpp
+++ src/Fraction/Fraction.cpp
@@ -1,70 +1,127 @@
-#include "Arion/Fraction/Fraction.hpp"
+#include <Arion/Fraction/Fraction.hpp>
 
 namespace Arion {
+	namespace Fraction {
+
 		// Constructs
-	Fraction::Fraction::Fraction(double Numerator, double Denominator) : Numerator(Numerator), Denominator(Denominator) {
-		if (Denominator == 0.f) {
-			throw std::exception("The denominator can't be equal to 0");
+		Fraction::Fraction(double Numerator, double Denominator) : Numerator(Numerator), Denominator(Denominator) {
+			if (Denominator == 0.f) {
+				throw std::exception("The denominator can't be equal to 0");
+			}
+
+			if (Denominator < 0) {
+				this->Numerator = -Numerator;
+				this->Denominator = -Denominator;
+			}
 		}
 
-		if (Denominator < 0) {
-			this->Numerator = -Numerator;
-			this->Denominator = -Denominator;
+		Fraction::Fraction(const Fraction& other) : Numerator(other.Numerator), Denominator(other.Denominator) {}
+
+		//Fraction::Fraction(Fraction&& other) noexcept;
+
+
+			// Setters
+		void Fraction::SetNumerator(double Numerator) {
+			this->Numerator = Numerator;
 		}
-	}
 
-	Fraction::Fraction::Fraction(Fraction& other) : Numerator(other.Numerator), Denominator(other.Denominator) {}
+		void Fraction::SetDenominator(double Denominator) {
+			if (Denominator == 0) {
+				throw std::exception("The denominator can't be equal to 0");
+			}
 
-	//Fraction::Fraction::Fraction(Fraction&& other) noexcept;
-
-
-		// Setters
-	void Fraction::Fraction::SetNumerator(double Numerator) {
-		this->Numerator = Numerator;
-	}
-
-	void Fraction::Fraction::SetDenominator(double Denominator) {
-		if (Denominator == 0) {
-			throw std::exception("The denominator can't be equal to 0");
+			if (Denominator < 0) {
+				Numerator = -Numerator;
+				this->Denominator = -Denominator;
+			}
+			else {
+				this->Denominator = Denominator;
+			}
 		}
-		
-		if (Denominator < 0) {
-			Numerator = -Numerator;
-			this->Denominator = -Denominator;
-		}
-		else {
-			this->Denominator = Denominator;
-		}
-	}
 
 
 		// Getter
-	double Fraction::Fraction::GetNumerator() const {
-		return Numerator;
-	}
+		double Fraction::GetNumerator() const {
+			return Numerator;
+		}
 
-	double Fraction::Fraction::GetDenominator() const {
-		return Denominator;
-	}
+		double Fraction::GetDenominator() const {
+			return Denominator;
+		}
 
 
 		// Other function
-	double Fraction::Fraction::Division() const noexcept{
-		return Numerator / Denominator;
-	}
-
-	void Fraction::Fraction::Reducing() {
-		while (std::floor(Numerator) != Numerator || std::floor(Denominator) != Denominator) {
-			Numerator *= 10;
-			Denominator *= 10;
+		double Fraction::Division() const noexcept {
+			return Numerator / Denominator;
 		}
 
-		auto gcd = std::gcd(std::abs((int)Numerator), std::abs((int)Denominator));	
-		Numerator /= gcd;
-		Denominator /= gcd;
-	}
+		void Fraction::Reducing() {
+			while (std::floor(Numerator) != Numerator || std::floor(Denominator) != Denominator) {
+				Numerator *= 10;
+				Denominator *= 10;
+			}
 
-	void Fraction::Fraction::Show() const {
-		std::cout << Numerator << "/" << Denominator << std::endl;
+			auto gcd = std::gcd(std::abs((int)Numerator), std::abs((int)Denominator));
+			Numerator /= gcd;
+			Denominator /= gcd;
+		}
+
+		void Fraction::Show() const {
+			std::cout << Numerator << "/" << Denominator << std::endl;
+		}
+
+		// Operations with fracions
+		Fraction& Fraction::operator=(const Fraction& other)
+		{
+			if (this == &other) {
+				return *this;
+			}
+
+			this->Numerator = other.Numerator;
+			this->Denominator = other.Denominator;
+			return *this;
+		}
+
+		Fraction Fraction::operator-() const {
+			return Fraction{ -Numerator, Denominator };
+		}
+
+		Fraction& Fraction::operator++() {
+			Numerator += Denominator;
+			return *this;
+		}
+
+		Fraction Fraction::operator++(int) {
+			Fraction old = *this;
+			Numerator += Denominator;
+			return old;
+		}
+
+		Fraction& Fraction::operator--() {
+			Numerator -= Denominator;
+			return *this;
+		}
+
+		Fraction Fraction::operator--(int) {
+			Fraction old = *this;
+			Numerator -= Denominator;
+			return old;
+		}
+
+		Fraction Fraction::operator+(Fraction other) {
+			Fraction tempFrac{ this->Numerator * other.Denominator + other.Numerator * this->Denominator,
+								this->Denominator * other.Denominator
+			};
+			tempFrac.Reducing();
+			return tempFrac;
+		}
+
+		Fraction Fraction::operator-(Fraction other) {
+			Fraction tempFrac{ this->Numerator * other.Denominator - other.Numerator * this->Denominator,
+								this->Denominator * other.Denominator
+			};
+			tempFrac.Reducing();
+			return tempFrac;
+		}
 	}
 }
